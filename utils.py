0809adf517943fe6a83ef7181053fbd7c34c70e0
Dawid Szyjka <dawid.szyjkaa1910@interia.pl>
2026-03-07 20:12:50 +0100

    refactor: extract train_model function to reduce duplication

--- utils.py
+++ utils.py
@@ -25,6 +25,55 @@ class MedicalDataset(Dataset):
 
         return image, label
     
+def train_model(model, optimizer, criterion, train_loader, test_loader, num_epochs, device):
+    lowest_loss = np.inf
+    best_model_weights = {k : v.cpu() for k, v in model.state_dict().items()}
+
+    for epoch in range(num_epochs):
+        print(f'Epoch: {epoch+1} / {num_epochs}')
+
+        for phase in ('train', 'val'):
+            if phase == 'train':
+                dataloader = train_loader
+                model.train()
+            else:
+                dataloader = test_loader
+                model.eval()
+
+            running_loss = 0.0
+            running_corrects = 0.0
+            total_samples = 0
+
+            for inputs, labels in dataloader:
+                inputs, labels = inputs.to(device), labels.to(device)
+
+                optimizer.zero_grad()
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, pred = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels.view(-1))
+
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
+
+                running_loss += loss.item() * labels.size(0)
+                running_corrects += (pred == labels.squeeze()).sum().item()
+                total_samples += labels.size(0)
+
+            epoch_loss = running_loss / total_samples
+            epoch_acc = running_corrects / total_samples
+
+            print(f'Loss: {epoch_loss}, accuracy: {epoch_acc}')
+
+            if phase == 'val' and epoch_loss < lowest_loss:
+                print(f'New lowest loss. Epoch {epoch+1}')
+                lowest_loss = epoch_loss
+                best_model_weights = {k : v.cpu() for k, v in model.state_dict().items()}
+
+    return best_model_weights   
+
+
 def evaluate_model(model, device, loader):
     y_pred, y_true = [], []
 
