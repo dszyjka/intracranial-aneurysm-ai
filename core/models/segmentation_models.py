68b4fec7043fc299fb3bd726110bd0426df8b05f
Dawid Szyjka <dawid.szyjkaa1910@interia.pl>
2026-04-09 01:48:24 +0200

    feat: add V-Net encoder implementation

--- core/models/segmentation_models.py
+++ core/models/segmentation_models.py
@@ -12,8 +12,8 @@ class ResidualBlock(nn.Module):
             layers.extend(
                 (
                     nn.Conv3d(channels, channels, kernel_size=5, stride=1, padding='same'), # here 'same' = 2
-                    nn.BatchNorm3d(channels),
-                    nn.PReLU()
+                    nn.InstanceNorm3d(channels),
+                    nn.PReLU(channels)
                 )
             )
 
@@ -24,5 +24,55 @@ class ResidualBlock(nn.Module):
         x = self.block(x)
         return x + res
 
-if __name__ == '__main__':
-    res = ResidualBlock(32, 4)
+class VNet(nn.Module):
+
+    def __init__(self, start_channels):
+        super().__init__()
+
+        self.stem_conv = nn.Sequential(
+            nn.Conv3d(1, start_channels, kernel_size=5, stride=1, padding='same'),
+            nn.InstanceNorm3d(start_channels),
+            nn.PReLU(start_channels)
+        )
+
+        self.res_blocks = nn.ModuleList([
+            ResidualBlock(start_channels, 1),
+            ResidualBlock(start_channels * 2, 2),
+            ResidualBlock(start_channels * 4, 3),
+            ResidualBlock(start_channels * 8, 3)
+        ])
+
+        self.down_convs = nn.ModuleList([
+            nn.Sequential(
+                nn.Conv3d(start_channels, start_channels * 2, kernel_size=2, stride=2),
+                nn.PReLU(start_channels * 2)
+            ),
+            nn.Sequential(
+                nn.Conv3d(start_channels * 2, start_channels * 4, kernel_size=2, stride=2),
+                nn.PReLU(start_channels * 4)
+            ),
+            nn.Sequential(
+                nn.Conv3d(start_channels * 4, start_channels * 8, kernel_size=2, stride=2),
+                nn.PReLU(start_channels * 8)
+            ),
+            nn.Sequential(
+                nn.Conv3d(start_channels * 8, start_channels * 16, kernel_size=2, stride=2),
+                nn.PReLU(start_channels * 16)
+            ),
+        ])
+
+        self.bottom_block = ResidualBlock(start_channels * 16, 3)
+
+    def forward(self, x):
+        horizontal_connections = []
+        
+        x = self.stem_conv(x)
+
+        for res_block, down_conv in zip(self.res_blocks, self.down_convs):
+            x = res_block(x)
+            horizontal_connections.append(x)
+            x = down_conv(x)
+
+        x = self.bottom_block(x)
+
+        return x
